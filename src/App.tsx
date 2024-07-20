23a9fde0e2e82f4180aedd841eb763f62e11f569
Muhammad Ali Akbar <maakbarofficial@gmail.com>
2024-07-20 20:11:20 +0500

    Building Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
 import Message from "./Message";
 
 function App() {
+  // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
+
+  const [alertVisiable, setAlertVisibable] = useState(false);
+
   return (
     <div>
-      <Message />
+      {/* <Message /> */}
+      {/* <ListGroup /> */}
+      {/* <ListGroup items={items} heading={"List"} onSelectItem={handleSelectItem} /> */}
+      {alertVisiable && <Alert onClose={() => setAlertVisibable(false)}>My alert</Alert>}
+      <Button color="danger" onClick={() => setAlertVisibable(true)}>
+        My Button
+      </Button>
     </div>
   );
 }
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let [selectedIndex, setSelectedIndex] = useState(-1);
+
+  // let selectedIndex = 0;
+
+  // items = [];
+
+  // bad practice
+  // if (items.length === 0)
+  //   return (
+  //     <>
+  //       <h1>List</h1>
+  //       <p>No item found</p>
+  //     </>
+  //   );
+
+  // const message = items.length === 0 ? <p>No item found</p> : null;
+
+  // const getMessage = () => {
+  //   return items.length === 0 ? <p>No item found</p> : null;
+  // };
+
+  // const handleClick = (event: MouseEvent) => console.log(event);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {/* {items.length === 0 ? <p>No item found</p> : null} */}
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className={selectedIndex === index ? "list-group-item active" : "list-group-item"}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
